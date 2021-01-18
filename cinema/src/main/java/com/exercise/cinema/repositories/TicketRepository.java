06dea310dcf5cd76f469d3d7723bc8d51e00ad6b
Bogdan Polak <bogdanpol@gmail.com>
2021-01-18 23:34:56 +0100

    Updated TicketRepository with findBySeat method

--- cinema/src/main/java/com/exercise/cinema/repositories/TicketRepository.java
+++ cinema/src/main/java/com/exercise/cinema/repositories/TicketRepository.java
@@ -2,6 +2,9 @@ package com.exercise.cinema.repositories;
 
 import com.exercise.cinema.models.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    @Query("SELECT t FROM tickets t WHERE t.RoomId = ?1 and t.RowNum = ?2 and t.SeatNum = ?3")
+    Ticket findBySeat(int RoomId, int RowNum, int SeatNum);
 }
