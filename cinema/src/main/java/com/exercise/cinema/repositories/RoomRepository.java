0711ac9ec0006b509f2d576e98bedd644e60992c
Bogdan Polak <bogdanpol@gmail.com>
2021-01-21 19:42:40 +0100

    Updated repositories keys

--- cinema/src/main/java/com/exercise/cinema/repositories/RoomRepository.java
+++ cinema/src/main/java/com/exercise/cinema/repositories/RoomRepository.java
@@ -3,5 +3,5 @@ package com.exercise.cinema.repositories;
 import com.exercise.cinema.models.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoomRepository extends JpaRepository<Room, Long> {
+public interface RoomRepository extends JpaRepository<Room, Integer> {
 }
